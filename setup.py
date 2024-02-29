d7d036011e2ea2361e71bb350da1ba20768a45d0
shuai <tonyyang1223@163.com>
2024-02-29 18:50:11 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quantscripts',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         # 在这里列出你的包的依赖项
